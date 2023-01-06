30a100141c728759639ea1d99020c4e3aaddc4f5
zLucasBiasi <lucasbiasi2001@gmail.com>
2023-01-06 17:39:11 -0300

    refactor: changing imports of index to components index

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+export { About } from "../components/About";
+export { ButtonTop } from "../components/ButtonTop";
+export { Contact } from "../components/Contact";
+export { HeadDocument } from "../components/Head";
+export { Header } from "../components/Header";
+export { Hero } from "../components/Hero";
+export { Projects } from "../components/Projects";
+export { Skills } from "../components/Skills";
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,15 @@
 import type { NextPage } from "next";
-import { useContext } from "react";
 
-import { About } from "../components/About";
-import { ButtonTop } from "../components/ButtonTop";
-import { Contact } from "../components/Contact";
-import { HeadDocument } from "../components/Head";
-import { Header } from "../components/Header";
-import { Hero } from "../components/Hero";
-import { Projects } from "../components/Projects";
-import { Skills } from "../components/Skills";
+import {
+    HeadDocument,
+    Header,
+    Hero,
+    About,
+    Skills,
+    Projects,
+    Contact,
+    ButtonTop,
+} from "../components";
 
 const Home: NextPage = () => {
     return (
