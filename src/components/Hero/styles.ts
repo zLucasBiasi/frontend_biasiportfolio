ecccdffaa2ab1b5c42dc91016117ff693f786f0e
zLucasBiasi <lucasbiasi2001@gmail.com>
2023-04-11 15:22:30 -0300

    feat: small responsiveness tweak on larger screens

--- src/components/Hero/styles.ts
+++ src/components/Hero/styles.ts
@@ -1,42 +1,40 @@
 import styled, { css } from "styled-components";
 
-export const ContentHero = styled.div`
-    margin-top: 7rem;
-    width: 100%;
-`;
-
 export const Wrapper = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: calc(100vh - 10rem);
 
-    @media (max-width: 1000px) {
-        flex-wrap: wrap;
-        flex-direction: column-reverse;
-    }
+  @media (max-width: 1000px) {
+    height: auto;
+    flex-wrap: wrap;
+    flex-direction: column-reverse;
+    margin-top: 6rem;
+  }
 `;
 
 export const Title = styled.h1`
-    ${({ theme }) => css`
-        font-size: ${theme.fonts.titleLarger};
-        font-weight: 600;
-        line-height: 7rem;
+  ${({ theme }) => css`
+    font-size: ${theme.fonts.titleLarger};
+    font-weight: 600;
+    line-height: 7rem;
 
-        @media (max-width: 1000px) {
-            font-size: ${theme.fonts.titleSmall};
-            text-align: center;
-            line-height: 5rem;
-        }
-        @media (max-width: 600px) {
-            font-size: ${theme.fonts.titleSmall};
-            text-align: center;
-            line-height: 5rem;
-        }
-    `}
+    @media (max-width: 1000px) {
+      font-size: ${theme.fonts.titleSmall};
+      text-align: center;
+      line-height: 5rem;
+    }
+    @media (max-width: 600px) {
+      font-size: ${theme.fonts.titleSmall};
+      text-align: center;
+      line-height: 5rem;
+    }
+  `}
 `;
 
 export const Paragraph = styled.p`
-    ${({ theme }) => css`
+  ${({ theme }) => css`
     font-size: ${theme.fonts.paragraphMedium};
     color: ${theme.colors.gray};
    
@@ -66,16 +64,16 @@ export const Paragraph = styled.p`
   `}
 `;
 export const Span = styled.span`
-    ${({ theme }) => css`
-        font-size: ${theme.fonts.titleExtraLarger};
-        color: ${theme.colors.blueCian};
-        font-weight: 700;
-        @media (max-width: 1000px) {
-            font-size: ${theme.fonts.titleLarger};
-        }
+  ${({ theme }) => css`
+    font-size: ${theme.fonts.titleExtraLarger};
+    color: ${theme.colors.blueCian};
+    font-weight: 700;
+    @media (max-width: 1000px) {
+      font-size: ${theme.fonts.titleLarger};
+    }
 
-        @media (max-width: 330px) {
-            font-size: ${theme.fonts.titleMedium};
-        }
-    `}
+    @media (max-width: 330px) {
+      font-size: ${theme.fonts.titleMedium};
+    }
+  `}
 `;
