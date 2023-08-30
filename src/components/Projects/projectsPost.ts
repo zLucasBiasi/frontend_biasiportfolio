801391b09aa723b709b5c432c84676cfa16ab291
Lucas Biasi <87048120+zLucasBiasi@users.noreply.github.com>
2023-08-30 00:59:17 -0300

    feat: add project

--- src/components/Projects/projectsPost.ts
+++ src/components/Projects/projectsPost.ts
@@ -1,5 +1,6 @@
 import projectTrampoDeCasa from "/public/assets/PosterTrampoDeCasa.webp";
 import projectSerpens from "/public/assets/PosterSerpens.webp";
+import projectNavalha from "/public/assets/projectNavalha.webp";
 
 export const Project = [
     {
@@ -7,7 +8,11 @@ export const Project = [
         poster: projectTrampoDeCasa,
         link: "https://trampodecasa.com.br/",
     },
-
+    {
+        name: "Biblioteca Navalha Dourada",
+        poster: projectNavalha,
+        link: "https://www.serpens.com.br/",
+    },
     {
         name: "Serpens Site",
         poster: projectSerpens,
